777a79131d5af7c73a95c3705fa4087d93e66bde
Cholthicha02 <cholthicha.cbf@gmail.com>
2024-04-22 16:15:30 +0700

    commit front-end

---
+++ front-end/src/store/index.js
@@ -0,0 +1,20 @@
+
+import { createStore } from 'vuex'
+
+import datafromApiFree from './modules/datafromApiFree'
+
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    datafromApiFree,
+
+  }
+})
+
---
+++ front-end/src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="auto">
+        <v-btn size="x-small">Extra small Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn size="small">Small Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn>Regular Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn size="large">Large Button</v-btn>
+      </v-col>
+
+      <v-col cols="auto">
+        <v-btn size="x-large">X-Large Button</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+  <h1 className="text-3xl font-bold underline text-orange-500">
+      Hello world! 
+
+    </h1>
+  </main>
+</template>
