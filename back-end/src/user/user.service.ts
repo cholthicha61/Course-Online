753bf98c25f9e8c18d60d8b1662096d94c90e4c2
PhongsaphatAunasri <phongsaphat.a@kkumail.com>
2024-04-22 17:19:09 +0700

    fix class UserService

--- back-end/src/user/user.service.ts
+++ back-end/src/user/user.service.ts
@@ -4,6 +4,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'john',
+      password: 'changeme',
+    },
+    {
+      userId: 2,
+      username: 'maria',
+      password: 'guess',
+    },
+  ];
+
+  async findOneTest(username: string) {
+    return this.users.find((user) => user.username === username);
+  }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
