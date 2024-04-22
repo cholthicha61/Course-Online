20e093520527dbbd682b96d5db974ffbb3f75a23
PhongsaphatAunasri <phongsaphat.a@kkumail.com>
2024-04-22 15:45:02 +0700

    feat enable cors

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
-config()
-const { PORT } = process.env
+config();
+const { PORT } = process.env;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    logger: ['log', 'debug', 'error', 'warn'],
+  });
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(PORT);
   console.log('server run on', PORT);
-
 }
+
 bootstrap();
