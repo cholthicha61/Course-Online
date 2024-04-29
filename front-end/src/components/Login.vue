c2bc69cbfa7a15128800785bdc679925598c360f
thammaphum <thammaphum.p@kkumail.com>
2024-04-29 17:51:31 +0700

    fe: test for P'Q

--- front-end/src/components/Login.vue
+++ front-end/src/components/Login.vue
@@ -2,15 +2,14 @@
     <div class="container mx-auto max-w-sm mt-16">
       <h2 class="text-3xl font-bold text-center">Login Account</h2>
   
-      <form @submit.prevent="handleSubmit">
         <div class="mt-6">
           <label for="email" class="block mb-2">Email</label>
-          <input type="email" id="email" v-model="email" class="w-full p-2 border rounded-md focus:outline-none">
+          <input type="email" id="email" v-model="payload.email" class="w-full p-2 border rounded-md focus:outline-none">
         </div>
   
         <div class="mt-6">
           <label for="password" class="block mb-2">Password</label>
-          <input type="password" id="password" v-model="password" class="w-full p-2 border rounded-md focus:outline-none">
+          <input type="password" id="password" v-model="payload.password" class="w-full p-2 border rounded-md focus:outline-none">
         </div>
   
         <div class="mt-4 flex items-center">
@@ -23,11 +22,11 @@
         </div>
   
         <div class="mt-6">
-          <button type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">
+          <button @click="Login()" type="submit" class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors">
             Login
           </button>
         </div>
-      </form>
+
   
       <div class="mt-6 text-center">
         <p>Don't have an account? <a href="#" class="text-blue-500 hover:text-blue-600">Create Account</a></p>
@@ -39,9 +38,11 @@
   export default {
     data() {
       return {
-        email: '',
-        password: '',
-        savePassword: false,
+        payload: {
+          email: '',
+          password: ''
+        }
+        
       };
     },
   
@@ -53,8 +54,12 @@
   
         this.email = '';
         this.password = '';
-        this.savePassword = false;
+        // this.savePassword = false;
       },
+      async Login(){
+        await this.$store.dispatch('auth/Login',this.payload)
+
+      }
     },
   };
   </script>
