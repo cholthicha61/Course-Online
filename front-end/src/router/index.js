6f966f236cb04b48c212c4abedcced21814afea9
Cholthicha02 <cholthicha.cbf@gmail.com>
2024-05-10 16:40:30 +0700

    fe: router

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+const routes =  [
     {
       path: "",
       component: () => import("../layout/BaseLaoutHome.vue"),
@@ -11,51 +9,61 @@ const router = createRouter({
           path: "/homepage",
           name: "homepage",
           component: () => import("../views/Main.vue"),
+          meta: { requiresGuest: true },
         },
         {
           path: "/allcourse-user",
           name: "allcourse-user",
           component: () => import("../components/AllCourse.vue"),
+          meta: { requiresGuest: true },
         },
         {
           path: "/addcourse",
           name: "addcourse",
           component: () => import("../views/AddCourse.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/inbox",
           name: "inbox",
           component: () => import("../views/Inbox.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/changePassword",
           name: "changePassword",
           component: () => import("../views/ChangePassword.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/coursemanage",
           name: "coursemanage",
           component: () => import("../views/CourseManage.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/allconfriem",
           name: "allconfriem",
           component: () => import("../views/AllconfrimedCourse.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/editcourse",
           name: "editcourse",
           component: () => import("../components/EditCourse.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/usermanage",
           name: "usermanage",
           component: () => import("../views/UserManage.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/category",
           name: "category",
           component: () => import("../views/Category.vue"),
+          meta: { requiresAuth: true },
         },
       ],
     },
@@ -67,19 +75,21 @@ const router = createRouter({
           path: "/home",
           name: "home",
           component: () => import("../views/Main.vue"),
+          meta: { requiresAuth: true },
         },
 
         {
           path: "/dashboard",
           name: "dashboard",
           component: () => import("../views/Dashboard.vue"),
+          meta: { requiresAuth: true },
         },
         {
           path: "/allcourse",
           name: "allcourse",
           component: () => import("../components/AllCourse.vue"),
+          meta: { requiresAuth: true },
         },
-        
       ],
     },
     {
@@ -90,15 +100,43 @@ const router = createRouter({
           path: "/login",
           name: "login",
           component: () => import("../views/Login.vue"),
+          meta: { requiresGuest: true },
         },
         {
           path: "/register",
           name: "register",
           component: () => import("../views/Register.vue"),
+          meta: { requiresGuest: true },
         },
       ],
     },
-  ],
+  ]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+const isAuthenticated = () => {
+  const token = localStorage.getItem("accessToken");
+  console.log("token",token);
+  return token !== null && token !== undefined;
+};
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth) && !isAuthenticated()) {
+    next({ name: 'login' });
+  } else if (to.matched.some(record => record.meta.requiresGuest) && isAuthenticated()) {
+    const role = localStorage.getItem("role");
+    if (role == 'user') {
+      next({ name: 'home' });
+
+    } else {
+      next({ name: 'dashboard' });
+
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
