3d238c919c1cd8fd21cf64d9f1d1c4b9c321c046
Toei <yarnnawit.r@kkumail.com>
2024-05-31 11:00:33 +0700

    fe : update-check-acount.disable

--- front-end/src/layout/BaseLayout.vue
+++ front-end/src/layout/BaseLayout.vue
@@ -5,7 +5,7 @@
         <NavbarLogin />
       </div>
     </div>
-    <div class=""> 
+    <div class="">
       <div v-if="role == 'admin'">
         <MasterVue />
       </div>
@@ -21,6 +21,9 @@
 import MasterVue from "./Master.vue";
 import Footers from "./Footers.vue";
 import NavbarLogin from "@/components/NavbarLogin.vue";
+import { mapState } from "vuex";
+import Swal from "sweetalert2";
+
 
 export default {
   components: {
@@ -31,14 +34,49 @@ export default {
   data() {
     return {
       role: localStorage.getItem("role"),
+      userNow: JSON.parse(localStorage.getItem('user'))
     };
   },
-  mounted() {
+  computed: mapState({
+    user: (state) => state.user.user,
+  }),
+
+  async mounted() {
     console.log("Role:", this.role);
+    await this.getData();
+    await this.checkUserActive()
   },
+  methods: {
+    async getData() {
+      await this.$store.dispatch("user/getUser");
+      console.log('userrr', this.user);
+    },
+    async checkUserActive() {
+      console.log('checkUserActive', this.userNow.id);
+      for (const item of this.user) {
+        if (item.id == this.userNow.id && item.active == false) {
+          console.log('okokok');
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Your account has been disabled.",
+          });
+          await this.$store.dispatch("auth/logout");
+          // window.location.reload();
+          // this.$router.push("/homepage");
+        }
+      }
+    },
+    watch: {
+      async user() {
+        console.log('User data changed, calling checkUserActive...');
+        await this.checkUserActive();
+      }
+    },
+
+  },
+
 };
 </script>
 
-<style scoped>
-/* Tailwind CSS utilities will handle most of the styling */
-</style>
+<style scoped></style>
