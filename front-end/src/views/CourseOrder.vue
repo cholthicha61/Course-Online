b95e179c3e876710e21bb9cd2fb99b7beb4a39c1
thidapat117 <thidapat.n@kkumail.com>
2024-05-31 16:19:51 +0700

    fix price order by orderdate

--- front-end/src/views/CourseOrder.vue
+++ front-end/src/views/CourseOrder.vue
@@ -20,7 +20,7 @@
                 }}
               </td>
               <td>
-                {{ item.course ? formatPrice(item.order.price) : "None" }}
+                {{ item.price ? formatPrice(item.price) : "None" }}
               </td>
               <td style="text-align: center; min-width: 120px">
                 <v-btn
